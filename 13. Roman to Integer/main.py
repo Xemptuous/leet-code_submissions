478ea55a7d875fbbac58db4716420a07a9118d70
Xemptuous <99044751+Xemptuous@users.noreply.github.com>
2023-02-04 17:25:07 -0800

    cleaned up code for clarity

--- 13. Roman to Integer/main.py
+++ 13. Roman to Integer/main.py
@@ -1,23 +1,24 @@
+ROMANS = {
+    "M": 1000,
+    "D": 500,
+    "C": 100,
+    "L": 50,
+    "X": 10,
+    "V": 5,
+    "I": 1
+}
+        
 class Solution:
     def romanToInt(self, s: str) -> int:
-        d = {
-            "M": 1000,
-            "D": 500,
-            "C": 100,
-            "L": 50,
-            "X": 10,
-            "V": 5,
-            "I": 1
-        }
         result = 0
-        for i in range(len(s) - 1, -1, -1):
-            if s[i] == "C":
-                result += d[s[i]] * (-1 if result >= 500 else 1)
-            elif s[i] == "X":
-                result += d[s[i]] * (-1 if result >= 50 else 1)
-            elif s[i] == "I":
-                result += d[s[i]] * (-1 if result >= 5 else 1)
+        for ch in s[::-1]:
+            if ch == "C":
+                result += ROMANS[ch] * (-1 if result >= 500 else 1)
+            elif ch == "X":
+                result += ROMANS[ch] * (-1 if result >= 50 else 1)
+            elif ch == "I":
+                result += ROMANS[ch] * (-1 if result >= 5 else 1)
             else:
-                result += d[s[i]]
+                result += ROMANS[ch]
         return result
             
